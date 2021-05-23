10dbf5730d8c283a96eed5275990c44c9a83070d
Peter Vecera <vecerapeto@gmail.com>
2021-05-23 15:11:37 +0200

    zakladne subory temy lekcia 9

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<div id="page" class="site">
+		<header>
+			<section class="search">Search</section>
+			<section class="top-bar">
+				<div class="brand">Logo</div>
+				<div class="second-column">
+					<div class="account">Account</div>
+					<nav class="main-menu">Menu</nav>
+				</div>
+			</section>
+		</header>
+		<div class="content-area">
+			<main>
+				<section class="slider">Slider</section>
+				<section class="popular-products">Popular Products</section>
+				<section class="new-arrivals">New Arrivals</section>
+				<section class="deal-of-the-week">Deal of tehe Week</section>
+				<section class="lab-blog">News</section>
+			</main>
+		</div>
+		<footer>
+			<section class="footer-widgets">Footer Widget</section>
+			<section class="copyright">Copyright</section>
+		</footer>
+	</div>
+
+</body>
+</html>
